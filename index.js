dbacd5d921f2e42b0d649627f906f908f9ffd9f7
DESKTOP-8NSKLP7\Administrator <1249725368@qq.com>
2019-09-01 12:20:52 +0800

    初始完成

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+const path = require('path')
+
+// 相对路径
+const svgDefaultPath = './src'
+
 module.exports = (api, projectOptions) => {
+  console.log('projectOptions ====> ', projectOptions)
+  const {
+    svgPath = svgDefaultPath,
+    symbolId = 'icon-[name]'
+  } = projectOptions.pluginOptions
   api.chainWebpack(webpackConfig => {
     // 通过 webpack-chain 修改 webpack 配置
+    const svgRule = webpackConfig.module.rule('svg') // 找到svg-loader
+    svgRule.uses.clear() // 清除已有的loader, 如果不这样做会添加在此loader之后
+    svgRule.exclude.add(/node_modules/) // 正则匹配排除node_modules目录
+    svgRule // 添加svg新的loader处理
+      .test(/\.svg$/)
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        include: path.resolve(__dirname, svgPath), // 只带自己人玩
+        symbolId
+      })
   })
 
   api.configureWebpack(webpackConfig => {
